95b6e81a6ef08e4f21194bfc046b3d245000b464
Enstrayed <48845980+Enstrayed@users.noreply.github.com>
2024-06-13 13:58:57 -0700

    switch /cider call to /nowplaying

--- static/calls.js
+++ static/calls.js
@@ -1,3 +1,3 @@
-fetch("https://api.enstrayed.com/cider").then(response => response.text()).then(response => {document.getElementById("nowplaying").innerHTML = response}) 
+fetch("https://api.enstrayed.com/nowplaying?format=html").then(response => response.text()).then(response => {document.getElementById("nowplaying").innerHTML = response}) 
 
 fetch("https://api.enstrayed.com/blogposts?format=html").then(response => response.text()).then(response => {document.getElementById("blogposts").innerHTML += response}) 
