fdbc1ffe78b5c3ce7145af7cf212a2f45a6e5604
Enstrayed <48845980+Enstrayed@users.noreply.github.com>
2024-08-21 17:26:39 -0700

    changes to conincide with major changes to api

--- static/calls.js
+++
@@ -1,3 +0,0 @@
-fetch("https://api.enstrayed.com/nowplaying?format=html").then(response => response.text()).then(response => {document.getElementById("nowplaying").innerHTML = response}) 
-
-fetch("https://api.enstrayed.com/blogposts?format=html").then(response => response.text()).then(response => {document.getElementById("blogposts").innerHTML += response}) 
